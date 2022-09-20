05039dd67c3af34bf6803cf0494115e1511cf650
KEA <lena_konovalova@rambler.ru>
2022-09-20 15:09:12 +0300

    z58

---
+++ ceminar_8/задача_56/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine("введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[m, n];
+FillArray(numbers);
+Console.WriteLine();
+Console.WriteLine("Массив до изменения");
+PrintArray(numbers);
+
+
+{
+    int minsum = Int32.MaxValue;
+    int line = 0;
+
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            sum = sum + numbers[i, j];
+        }
+        if (sum < minsum)
+        {
+            minsum = sum;
+            line++;
+        }
+    }
+    Console.WriteLine("строка с наименьшей суммой елементов под номером: " + (line) + ", с суммой елементов равной: " + (minsum));
+}
+
+
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 20);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+
+        Console.WriteLine("");
+    }
+}
---
+++ ceminar_8/задача_58/Program.cs
@@ -0,0 +1,66 @@
+﻿Console.WriteLine("введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] matrix = new int[m, n];
+int[,] matrix2 = new int[m, n];
+int[,] result = new int[m,n];
+FillArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Матрица 1");
+PrintArray(matrix);
+FillArray(matrix2);
+Console.WriteLine();
+Console.WriteLine("Матрица 2");
+PrintArray(matrix2);
+
+
+if (matrix.GetLength(0) != matrix2.GetLength(1))
+{
+    Console.WriteLine("Операция не возможна! ");
+    
+}
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix2.GetLength(1); j++)
+    {
+        result[i, j] = 0;
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            result[i, j] += matrix[i, k] * matrix2[k, j];
+        }
+    }
+
+}
+
+
+
+Console.WriteLine("Результат ");
+PrintArray(result);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+
+        Console.WriteLine("");
+    }
+}
